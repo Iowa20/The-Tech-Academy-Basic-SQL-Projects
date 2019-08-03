b6b295a35c4d93e2be15a24528ccce24051f6160
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-03 13:26:24 -0400

    update

---
+++ drill7.sql
@@ -0,0 +1,41 @@
+
+
+
+
+CREATE TABLE JiuJitsu_specialist (
+bjj_id INT PRIMARY KEY not NULL IDENTITY (1,1),
+bjj_lastname VARCHAR(50) not NULL,
+bjj_firstname VARCHAR(50) not NULL);
+
+SELECT * FROM JiuJitsu_specialist;
+
+
+INSERT INTO JiuJitsu_specialist
+(bjj_lastname, bjj_firstname)
+VALUES
+('Garcia', 'Marcelo'),
+('Ribeiro', 'Xande'),
+('Galvao', 'Andrei'),
+('Bravo', 'Eddie');
+
+CREATE TABLE Bjj_academy (
+academy_name VARCHAR(50) not null,
+technique_name VARCHAR(50) not NULL);
+
+INSERT INTO Bjj_academy
+(academy_name, technique_name)
+VALUES
+('MG Academy','Butterfly Guard'),
+('Jiu Jitsu University','Uchi Mata'),
+('Atos', 'Rear Naked Choke'),
+('10th Planet BJJ', 'Electric chair');
+
+
+
+SELECT bjj_id, bjj_lastname, bjj_firstname,
+academy_name, technique_name
+FROM JiuJitsu_specialist 
+FULL OUTER JOIN Bjj_academy 
+ON bjj_firstname = academy_name;
+
+
