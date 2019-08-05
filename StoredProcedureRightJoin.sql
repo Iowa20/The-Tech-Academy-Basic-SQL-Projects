098533a67b8105fddf63f3fa610f5f1bbfe9601c
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-04 20:37:59 -0400

    update

--- StoredProcedureRightJoin.sql
+++ StoredProcedureRightJoin.sql
@@ -1,13 +1,6 @@
+
+
 USE [AdventureWorks2014]
 GO
 
-EXEC 
-
-
-
-SELECT * FROM Person.EmailAddress;
-SELEct * FROM Person.Password;
-
-
-SELECT Person.Password.BusinessEntityID, Person.EmailAddress.BusinessEntityID,Person.EmailAddress.EmailAddressID
-FROM 
+EXEC [dbo].[usoGetBusinessIdEmailAddressFullJoin]
--- drill 4.sql
+++ drill 4.sql
@@ -1,4 +1,12 @@
 USE [db_zootest2]
+GO
 
-SELECT species_name, nutrition_id FROM tbl_species, tbl_nutrition
+SELECT species_name, nutrition_id 
+FROM tbl_species
+INNER JOIN tbl_nutrition
+ON
+tbl_nutrition.nutrition_id = tbl_species.species_nutrition
 WHERE nutrition_id BETWEEN 2202 and 2206;
+
+SELECT * FROM tbl_species;
+SELECT * FROM tbl_nutrition;
