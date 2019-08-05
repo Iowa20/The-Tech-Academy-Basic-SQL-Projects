1e20f6f2109387537173b222bb2e3f52f533841f
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-04 21:07:00 -0400

    update

--- drill 5.sql
+++ drill 5.sql
@@ -1,4 +1,15 @@
 USE [db_zootest2]
+GO
 
-SELECT species_name AS "Species Name", nutrition_type AS "Nutrition Type" 
-FROM tbl_species, tbl_nutrition;
+SELECT species_name AS "Species Name", nutrition_type AS "Nutrition Type"
+FROM tbl_species
+INNER JOIN tbl_nutrition
+ON 
+tbl_nutrition.nutrition_id = tbl_species.species_nutrition;
+
+
+
+
+
+SELECT * FROM tbl_species;
+SELECT * FROM tbl_nutrition;
