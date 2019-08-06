28a5cac28c97f43d456c1514467f527e66df6a8a
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-06 19:13:15 -0400

    update

--- drill 6.sql
+++ drill 6.sql
@@ -1,10 +1,17 @@
 USE [db_zootest2]
+GO
 
-SELECT specialist_fname, specialist_lname, specialist_contact
-FROM tbl_specialist
-WHERE specialist_id = 3;
+
 
 
 SELECT * FROM tbl_specialist
 SELECT * FROM tbl_species
 SELECT * FROM tbl_care
+
+
+SELECT specialist_fname AS "first name", specialist_lname AS "last name", specialist_contact AS "contact"
+FROM tbl_specialist
+INNER JOIN tbl_species
+ON tbl_specialist.specialist_id = tbl_species.species_id
+INNER JOIN tbl_care
+ON tbl_species.species_id = tbl_care.care_specialist;
