1d65f87d32446e0426b2e357d54fb5f5b25b9e57
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-07 18:51:19 -0400

    update

--- drill 6.sql
+++ drill 6.sql
@@ -6,12 +6,15 @@ GO
 
 
 
-SELECT specialist_fname AS "first name", specialist_lname AS "last name", specialist_contact AS "contact"
+SELECT specialist_fname, specialist_lname, specialist_contact, care_specialist, species_name
 FROM tbl_specialist
-INNER JOIN tbl_species
-ON tbl_specialist.specialist_id = tbl_species.species_id
 INNER JOIN tbl_care
-ON tbl_species.species_id = tbl_care.care_specialist;
+ON tbl_specialist.specialist_id = tbl_care.care_specialist
+INNER JOIN tbl_species
+ON tbl_care.care_id = tbl_species.species_care
+WHERE species_name = 'penguin';
+
+
 
 SELECT * FROM tbl_specialist
 SELECT * FROM tbl_species
